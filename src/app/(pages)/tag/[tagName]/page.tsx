b038ac15a15f0c25d9912f6ff5a6efc1707ff41c
JSBzz <101786987+JSBzz@users.noreply.github.com>
2024-08-21 16:31:32 +0900

    nickname error

--- src/app/(pages)/tag/[tagName]/page.tsx
+++ src/app/(pages)/tag/[tagName]/page.tsx
@@ -1,5 +1,11 @@
 import Main from "@/app/_components/Main/Main";
 
-export default async function Home({ params }: { params: { tagName: string } }) {
-  return <Main categoryCode="ALL" tagName={params?.tagName} />;
+export default async function Home({
+  params,
+  searchParams,
+}: {
+  params: { tagName: string };
+  searchParams: any;
+}) {
+  return <Main categoryCode="ALL" tagName={params?.tagName} searchParam={searchParams?.q} />;
 }
